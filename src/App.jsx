8029ddf8af94dbfb24cbc4b96198cf886bd954ae
Shivam-srm <shivamrai83170@gmail.com>
2026-04-14 23:22:40 +0530

    Mac-Os

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './app.scss'
+import Dock from './components/Dock'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <main>
+      <Nav />
+      <Dock />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './nav.scss'
+
+const Nav = () => {
+  return (
+   <nav>
+        <div className="left"> 
+            <div className="apple-icon">
+                <img src="./navbar-icons/apple.svg" alt="Apple" />
+            </div>
+        
+        
+        <div className="nav-item">
+            <p>Shivam Rai</p>
+        </div>
+
+        <div className="nav-item">
+            <p>File</p>
+        </div>
+
+        <div className="nav-item">
+            <p>Window</p>
+        </div>
+
+        <div className="nav-item">
+            <p>Terminal</p>
+        </div>
+        </div>
+        <div className="right">
+           <div className="nav-icon">
+            <img src="/navbar-icons/wifi.svg" alt="wifi" />
+           </div>
+            <div className="nav-item">
+           <DateTime />
+        </div>
+            </div>
+        
+   </nav>
+  )
+}
+
+export default Nav
