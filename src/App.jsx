423a1f0ef083721c29ab9ac9205d04377eeb77b2
Shivam-srm <shivamrai83170@gmail.com>
2026-04-15 00:31:35 +0530

    terminal

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './app.scss'
 import Dock from './components/Dock'
 import Nav from './components/Nav'
+import MacWindow from './components/windows/MacWindow'
 
 const App = () => {
   return (
     <main>
       <Nav />
+      <MacWindow />
       <Dock />
     </main>
   )
---
+++ src/components/windows/MacWindow.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Rnd } from 'react-rnd'
+import './macwindow.scss'
+
+const MacWindow = (children) => {
+  return (
+   <Rnd>
+    <div className="window">
+      <div className="nav">
+        <div className="dots">
+            <div className="dot red"></div>
+            <div className="dot yellow"></div>
+            <div className="dot green"></div>
+        </div>
+
+            <div className="title"><p>shivamrai - zsh</p></div>
+      </div>
+      <div className="main-content">
+        {children}
+      </div>
+    </div>
+    </Rnd>
+  )
+}
+
+export default MacWindow
